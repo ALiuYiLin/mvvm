a18743089ef180b6eae321241c13efc132b78b7e
ALinYiLin <scliuyilin@163.com>
2026-02-07 22:29:12 +0800

    feat: watchEffect函数添加；监视函数响应式数据变化，一但变化就执行回调函数

---
+++ src/core/watchEffect.ts
@@ -0,0 +1,11 @@
+import { setCurrentUpdateFn } from "./state"
+
+/**
+ * 监听响应式数据的变化，当数据变化时，调用回调函数
+ * @param callback 
+ */
+export function watchEffect(callback: () => void) {
+  setCurrentUpdateFn(callback)
+  callback()
+  setCurrentUpdateFn(null)
+}
