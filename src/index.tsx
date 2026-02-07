bc36b27f56a727e5c593d460e7f066d13f562409
ALinYiLin <scliuyilin@163.com>
2026-02-07 23:05:50 +0800

    feat: 使用render函数，通过jsx生成DOM实现案例

---
+++ src/index.tsx
@@ -0,0 +1,106 @@
+// 主入口文件
+import { compile } from "./core/compile";
+import  { Option } from './types'
+import { ref } from "./core/ref";
+import "./style.css";
+import { reactive } from "./core/reactive";
+import { watchEffect } from "./core/watchEffect";
+import { computed } from "./core/computed";
+import { watch } from "./core/watch";
+
+// 1. 响应式数据
+// 2. 订阅发布模式
+// 3. 编译
+
+const count = ref(0);
+console.log('count: ', count);
+
+const student = reactive({
+  name: '张三',
+  age: 20
+})
+console.log('student: ', student);
+
+const studentList = reactive([
+  {
+    name: '张三',
+    age: 20
+  },
+  {
+    name: '张四',
+    age: 21
+  },
+  {
+    name: '张五',
+    age: 22
+  },
+])
+
+
+
+
+
+const nameAndAge = computed(()=>{
+  return '姓名：' + student.name + ' 年龄：' + student.age
+})
+
+function handlerClick(){
+  count.value++
+  student.age++
+  studentList.push({
+    name: 'aaaaa',
+    age: Math.random()
+  })
+}
+
+watch(count, (newValue, oldValue) => {
+  console.log('count 变化了', oldValue, '->', newValue);
+})
+watch(nameAndAge, (newValue, oldValue) => {
+  console.log('nameAndAge 变化了', oldValue, '->', newValue);
+})
+watch(student, (newValue, oldValue) => {
+  console.log('student 变化了', oldValue, '->', newValue);
+})
+watch(() => student.age, (newValue, oldValue) => {
+  console.log('student.age 变化了', oldValue, '->', newValue);
+})
+watchEffect(() => {
+  console.log('watchEffect student.age 变化了', student.age);
+})
+
+const options: Option[] = [
+  {
+    selector: "#count",
+    text: () => `count: ${count.value}`,
+  },
+  {
+    selector: '#counter',
+    listeners: [
+      {
+        type: 'click',
+        callback: handlerClick
+      }
+    ],
+    text: () => `点击计数: ${count.value}`
+  },
+  {
+    selector: '#student',
+    text: ()=> '姓名：' + student.name + ' 年龄：' + student.age
+  },
+  {
+    selector: '#student--list',
+    // text: ()=> studentList.map(item=>'姓名：' + item.name + ' 年龄：' + item.age).join('\n'),
+    render: ()=> (
+      <ul>
+        {studentList.map(item=><li>{'姓名：' + item.name + ' 年龄：' + item.age}</li>)}
+      </ul>
+    )
+  },
+  {
+    selector: '#lisi-info',
+    text: ()=> nameAndAge.value
+  },
+];
+
+options.forEach((option) => compile(option));
