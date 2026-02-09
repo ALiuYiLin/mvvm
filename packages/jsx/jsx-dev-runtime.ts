21810181a527c3bdd576e48a558a5ffc83259416
ALiuYiLin <scliuyilin@163.com>
2026-02-09 12:37:19 +0800

    feat: 模块配置更新

---
+++ packages/jsx/jsx-dev-runtime.ts
@@ -0,0 +1,6 @@
+/**
+ * JSX Dev Runtime - Development mode JSX runtime
+ * This is used when jsx: "react-jsxDEV" is configured
+ */
+export { createElement, createElement as jsx, createElement as jsxs, jsxDEV, Fragment } from "./jsx";
+export * from "./jsx";
