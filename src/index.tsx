f6bbee39d7762b48b6a929fd2442c7f91ed469f9
ALiuYiLin <scliuyilin@163.com>
2026-02-11 11:01:08 +0800

    feat: 路由切换demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,119 +1,71 @@
-// 主入口文件
-import "./style.css";
-import { ref, reactive, computed, compile, Option } from '@actview/core';
+import { App, computed, Option, reactive, ref } from "@actview/core";
 
+// 核心就这么多
+class Router {
+  routes: { path: string; component: () => string | HTMLElement | Text | DocumentFragment | SVGElement }[] = [
+    { path: '/home', component: () => <div>Home</div> },
+    { path: '/not-found', component: () => <div>Not Found</div>},
+  ];
+  
+  push(path: string) {
+    history.pushState(null, '', path);
+    this.update();
+  }
 
+  update() {
+    const path = location.pathname;
+    const route = this.routes.find(r => r.path === path);
+    if (route) route.component(); // 渲染匹配的组件
+  }
 
-// 1. 响应式数据
-// 2. 订阅发布模式
-// 3. 编译
-
-const count = ref(0);
-console.log('count: ', count);
-
-const student = reactive({
-  name: '张三',
-  age: 20
-})
-console.log('student: ', student);
-
-const studentList = reactive([
-  {
-    name: '张三',
-    age: 20
-  },
-  {
-    name: '张四',
-    age: 21
-  },
-  {
-    name: '张五',
-    age: 22
-  },
-])
-
-const type = ref('primary');
-
-
-
-const nameAndAge = computed(()=>{
-  return '姓名：' + student.name + ' 年龄：' + student.age
-})
-
-function handlerClick(){
-  count.value++
-  student.age++
-  studentList.push({
-    name: 'aaaaa',
-    age: Math.random()
-  })
-  if(count.value % 2 === 0) {
-    type.value = 'primary'
-  } else {
-    type.value = 'secondary'
+  init() {
+    window.addEventListener('popstate', () => this.update());
+    this.update();
   }
 }
 
+// const router = new Router();
+// router.init();
+
+const useRouter = () => {
+  const routes = [
+    { path: "/home", component: () => <div>Home</div> },
+    { path: "/not-found", component: () => <div>Not Found</div> },
+  ]
+
+  const currentRoutePath = ref('');
+  const route = computed(() => {
+    return routes.find(r => r.path === currentRoutePath.value);
+  })
+
+  function push(path: string) {
+    history.pushState(null, '', path);
+    currentRoutePath.value = path;
+  }
+
+  return {
+    route,
+    push,
+  }
+}
+
+
+const app = new App();
+const {route, push} = useRouter();
+
 const options: Option[] = [
   {
-    selector: "#count",
-    text: () => `count: ${count.value}`,
-  },
-  {
-    selector: '#counter',
-    listeners: [
-      {
-        type: 'click',
-        callback: handlerClick
-      }
-    ],
-    text: () => `点击计数: ${count.value}`
-  },
-  {
-    selector: '#student',
-    text: ()=> '姓名：' + student.name + ' 年龄：' + student.age
-  },
-  {
-    selector: '#student--list',
-    // text: ()=> studentList.map(item=>'姓名：' + item.name + ' 年龄：' + item.age).join('\n'),
-    render: ()=> (
-      <ul>
-        {studentList.map(item=><li>{'姓名：' + item.name + ' 年龄：' + item.age}</li>)}
-      </ul>
-    )
-  },
-  {
-    selector: '#lisi-info',
-    text: ()=> nameAndAge.value
-  },
-  {
-    selector: '#input-name',
-    listeners:[
-      {
-        type: 'input',
-        callback: (e) => {
-          student.name = (e.target as HTMLInputElement).value
-        }
-      }
-    ]
-  },
-  {
-    selector: '#user-form',
+    selector: "#app",
     render: () => (
-      <form>
-        <label>姓名：</label>
-        <input id="input-name" type="text" placeholder="请输入姓名"  onInput={(e: Event) => {
-          student.name = (e.target as HTMLInputElement).value
-        }}/>
-        <label>年龄：</label>
-        <input id="input-age" type="number" placeholder="请输入年龄" 
-          value={String(student.age)}
-          onInput={(e: Event) => {
-          student.age = Number((e.target as HTMLInputElement).value)
-        }}/>
-        <span>{student.age}</span>
-      </form>
+      <div>
+        <h1>欢迎来到ActView</h1>
+        <p>这是一个简单的示例</p>
+        <p>当前路由：{route.value?.path}</p>
+        <button onClick={() => push('/home')}>Home</button>
+        <button onClick={() => push('/not-found')}>Not Found</button>
+        {route.value?.component()}
+      </div>
     )
-  }
-];
-options.forEach((option) => compile(option));
+  },
+]
+app.resolveOptions(options)
--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
         button: path.resolve(__dirname, 'src/components/button/index.html'),
         switch: path.resolve(__dirname, 'src/components/switch/index.html'),
         input: path.resolve(__dirname, 'src/components/input/index.html'),
+        notfound: path.resolve(__dirname, 'src/notfound/index.html'),
       }
     }
   },
