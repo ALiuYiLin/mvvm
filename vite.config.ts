9c0920c926d9062eecff0bc1c0a111d2f96c35fc
ALiuYiLin <scliuyilin@163.com>
2026-02-12 10:51:01 +0800

    feat: 路径别名处理

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import path from 'path'
 
 export default defineConfig({
   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   },
   server: {
